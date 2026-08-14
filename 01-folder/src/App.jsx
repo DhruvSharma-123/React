dd961f286fa5e6dbef99f23e1a66dec4e840a32c
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-08-14 22:13:40 +0530

    React Start

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id="parent">
+      <h1>My name is Dhruv Sharma</h1>
+      <h6>Helo</h6>
+    </div>
+  )
+}
+
+export default App
+
