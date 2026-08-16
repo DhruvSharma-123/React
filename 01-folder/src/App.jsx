975d4bd64eda243c2689ef12ca84b723d69d169a
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-08-16 23:15:11 +0530

    PropsComplete

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Card from "./component/card"
 import Navbar from "./component/navbar"
-
+import spider from "./assets/spider.avif";
 const App = () => {
   return (
     <div id="parent">
-     <Card user="Dhruv Sharma" age={18}/>
-     <Card  user="Keshav Sharma" age={18}/>
+     <Card user="Dhruv Sharma" age={18} img={spider}/>
+     <Card  user="Keshav Sharma" age={18} img="https://images.unsplash.com/photo-1642456074142-92f75cb84533?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
   
     </div>
   )
--- 01-folder/src/component/card.jsx
+++ 01-folder/src/component/card.jsx
@@ -2,11 +2,11 @@
 //     return "HMM.";
 // }
 import React from "react";
-import spider from "../assets/spider.avif";
+
 function Card(props){
     console.log(props);
     return <div className="prop">
-    <img src={spider} alt="Spider man" />
+    <img src={props.img} alt="Spider man" />
 <h3>{props.user}</h3>       
       <h4>{props.age}</h4>
       </div>
