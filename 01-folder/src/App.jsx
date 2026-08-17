c9af48e0756b5a4122c308758a84541d706dc678
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-08-17 22:40:46 +0530

    UIunderprocess

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Card from "./component/card"
 import Navbar from "./component/navbar"
 import spider from "./assets/spider.avif";
+import Ui from "./component/ui";
+
 const App = () => {
   return (
     <div id="parent">
-     <Card user="Dhruv Sharma" age={18} img={spider}/>
+     {/* <Card user="Dhruv Sharma" age={18} img={spider}/>
      <Card  user="Keshav Sharma" age={18} img="https://images.unsplash.com/photo-1642456074142-92f75cb84533?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-  
+   */}
+<Ui/>
     </div>
   )
 }
---
+++ 01-folder/src/component/ui.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+function  Ui(){
+return <div className="Uip">
+<div className="uione">
+    <img  src="https://static.vecteezy.com/system/resources/previews/018/779/928/original/3d-illustration-of-amazon-logo-free-png.png" alt="amazon " />
+    <button id="bone">Save</button>
+</div>
+<div>
+    <span><span className="b">Amazon</span><small opicity={0.5} >  5 days ago</small></span>
+</div>
+<div>
+    <h5>Senior ux/ui Designer</h5>
+</div>
+<div id="ps">
+   <div className="part">Part Time </div>
+   <div className="Senior">Senior Level</div>
+</div>  
+<br />
+<br />
+<hr />
+<span>
+<b>$120/hr</b> <button>Apply now</button>
+</span>
+</div>
+}
+
+export default Ui;
