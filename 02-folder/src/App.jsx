833ad34ae98a3f2ddb696d67dce906dc3d05a81c
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-08-20 00:01:50 +0530

    Module css

---
+++ 02-folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from "./components/Button/Button/button.jsx"
+
+const App = () => {
+  return (
+    <div>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
