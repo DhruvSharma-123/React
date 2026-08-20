17b946b106ba16974d246c2109224606d009a52e
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-08-20 14:16:32 +0530

    Prac

--- 02-folder/src/App.jsx
+++ 02-folder/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Button from "./components/Button/Button/button.jsx"
-
+import Header from "./components/Button/Header/header.jsx"
 const App = () => {
   return (
     <div>
-      <Button/>
+      {/* <Button/> */}
+      <Header />
     </div>
   )
 }
---
+++ 02-folder/src/components/Button/Header/header.jsx
@@ -0,0 +1,10 @@
+import styles from "./header.module.css"
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
