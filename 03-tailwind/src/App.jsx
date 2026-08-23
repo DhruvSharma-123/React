1e83e3d6952dd2f2409590b798266eb444b5e8c2
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-08-23 12:47:13 +0530

    Next Ui using tailwind Under Process

---
+++ 03-tailwind/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Section1 from "./components/Section-1/section1";
+const App = () => {
+  return (
+    <div className='h-full w-full'>
+      {/* Har Har Mahadev */}
+      <Section1 />
+    </div>
+  )
+}
+
+export default App
---
+++ 03-tailwind/src/components/Section-1/LeftContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ArrowUpRight } from "lucide-react";
+const LeftContent = () => {
+  return (
+    <div className='h-full w-1/3 bg-blue-600 flex flex-col justify-between'>
+   <div className='p-6'>
+    <h3 className='text-5xl font-bold mb-5'>Prospective <br/>Customer <br/> Segmentation</h3>
+<p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus perferendis dolore laborum doloribus a laudantium enim doloremque sequi veniam praesentium.</p>
+   </div>
+   <div className='text-8xl'><ArrowUpRight /></div>
+    </div>
+  )
+}
+
+export default LeftContent
