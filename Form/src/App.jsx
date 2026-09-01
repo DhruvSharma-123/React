28a9ac00886b8e182911550dd2c6a0b4044fc7c1
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-09-01 15:50:06 +0530

    Process

--- Form/src/App.jsx
+++ Form/src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  function submit(e){
+    e.preventDefault();
+    SetNum("");
+          SetTnum("");
+
+
+  }
+  const [num,SetNum]=useState("");
+  const [tnum,SetTnum]=useState("");
   return (
-    <div>
-      App
+    <div className='bg-black'>
+      <form action="" onSubmit={(e)=>{    
+        submit(e);
+       
+
+        }}>
+<input type="text" placeholder='Enter Your Name' value={num} onChange={(e)=>{
+SetNum(e.target.value);
+}}></input>
+
+        <textarea type="text" placeholder='Give you feedback' value={tnum} onChange={(e)=>{
+          SetTnum(e.target.value);
+        }}></textarea>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
