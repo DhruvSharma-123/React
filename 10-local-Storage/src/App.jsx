16ba6d701fed9b64340a51b05dda56d3e8653292
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-09-05 16:06:50 +0530

    Local-Storage

---
+++ 10-local-Storage/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const App = () => {
+  //Local storage is the storage of browser in which if one time data is saved then it will save permanentaly until you remove it.like your login your account
+  // localStorage.clear();                               //Clear whole storage 
+  // localStorage.setItem("Name","Dhruc Sharma");        //Set Element in storage 
+  // localStorage.setItem("Age",18 );
+  // let name=localStorage.getItem("Name");            //Get element from local storage 
+  // console.log(name);
+  // localStorage.removeItem("Name");                 //Remove particular item
+ 
+ 
+ //In Local Storage data is store in the form of string 
+ //So we learn how obj saved in local strorage
+ const data={
+  name:"Dhruv Sharma",
+  age:18,
+  Branch:"A.I.M.L"
+ } 
+ localStorage.setItem("data",JSON.stringify(data));
+ let dataacc=JSON.parse(localStorage.getItem("data"));
+ console.log(dataacc)
+ 
+ 
+  return (
+    <div>
+        App.
+
+    </div>
+  )
+}
+
+export default App
