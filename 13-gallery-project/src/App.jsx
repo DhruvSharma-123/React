d7a6d225a8cd0ae4ba488834c309beb359c44f05
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-09-07 11:25:05 +0530

    Gallery Pro Comp

--- 13-gallery-project/src/App.jsx
+++ 13-gallery-project/src/App.jsx
@@ -4,11 +4,11 @@ import axios from 'axios'
 const App = () => {
   async function getdata(){
     console.log("DATA AA GYA ")
-    const respond=await axios.get("https://picsum.photos/v2/list?page=2&limit=30")
+    const respond=await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=30`)
    SetUserdata(respond.data);
    console.log(respond.data);
   }
-
+const [index,SetIndex]=useState(1);
 const [userdata,SetUserdata]=useState([]);
   let prisntUserdata=<h3 className='text-gray-400'>No data looking</h3>
 
@@ -28,10 +28,14 @@ return <div>
 
   useEffect(()=>{
       getdata();
-  })
+  },[index])
   return (
     <div className='bg-black h-screen text-white p-4 overflow-auto ' >
 <div className='bg-black text-white flex flex-wrap gap-2'>{prisntUserdata}</div>
+<div className='flex justify-center gap-6 items-center p-4'>
+  <button className='bg-amber-400 text-sm text-black rounded px-4 py-2 cursor-pointer active:scale-95' onClick={()=>{if(index>0){SetIndex(index-1)}}}>Prev</button>
+  <button className='bg-amber-400 text-sm text-black rounded px-4 py-2 cursor-pointer active:scale-95' onClick={()=>{SetIndex(index+1)}}>Next</button>
+</div>
     </div>
   )
 }
